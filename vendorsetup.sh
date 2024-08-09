a421756ac1babb5ea4fb28e28d9774d4b0bac77d
Darth9 <reyprimerjefe@gmail.com>
2024-08-09 21:18:35 +0100

    Remove redundant setting

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -48,7 +48,6 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
   export FOX_USE_SED_BINARY=1
   export FOX_USE_XZ_UTILS=1
   export FOX_DELETE_AROMAFM=1
-  export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800" # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
   export FOX_ENABLE_APP_MANAGER=0
 
   # let's see what are our build VARs
