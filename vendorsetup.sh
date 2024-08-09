4a2f3baac50e16ddc88ab3d0808e01471c133857
Darth9 <reyprimerjefe@gmail.com>
2024-08-09 21:18:35 +0100

    Remove redundant setting

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -52,7 +52,6 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
         export TARGET_DEVICE_ALT="sweetin"
 	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-        export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
 
 	# R11.2 Settings
 	export FOX_VERSION="R11.2-1"
