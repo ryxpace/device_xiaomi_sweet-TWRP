2f90895e72ecff86d5b03a5b36cabcb27667fe5f
Darth9 <reyprimerjefe@gmail.com>
2025-02-18 17:01:44 +0000

    Add the prebuilt 'date' binary

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -44,6 +44,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_USE_BASH_SHELL=1
 	export FOX_ASH_IS_BASH=1
 	export FOX_USE_TAR_BINARY=1
+	export FOX_USE_DATE_BINARY=1
 	export FOX_USE_XZ_UTILS=1
 	export FOX_USE_LZ4_BINARY=1
 	export FOX_USE_ZSTD_BINARY=1
