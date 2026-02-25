0899b50f28d5f48bfe709eb146d6b8896ee051de
Raynando Andre <ryxpace@proton.me>
2026-02-25 19:03:59 +0900

    sweet: update
    - device: move TW flags into BoardConfig.mk
    - twrp_sweet: inherit base and core64bit

--- twrp_sweet.mk
+++ twrp_sweet.mk
@@ -18,7 +18,9 @@
 PRODUCT_RELEASE_NAME := sweet
 DEVICE_PATH := device/xiaomi/sweet
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
