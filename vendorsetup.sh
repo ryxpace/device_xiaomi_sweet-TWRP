6b565128807d906e3539f799472784b7881a03b3
Raynando Andre <ryxpace@proton.me>
2026-03-27 21:16:24 +0900

    vendorsetup: remove sweet from TARGET_DEVICE_ALT
    leftover from initial vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -37,7 +37,7 @@ fi
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
   export LC_ALL="C"
   export ALLOW_MISSING_DEPENDENCIES=true
-  export TARGET_DEVICE_ALT="sweet,sweetin"
+  export TARGET_DEVICE_ALT="sweetin"
   export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
   export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
   export FOX_USE_NANO_EDITOR=1
