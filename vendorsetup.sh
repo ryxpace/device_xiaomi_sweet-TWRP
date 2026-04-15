4ad55ca137306a27d80a46a48103b2d35fd89041
Raynando Andre <ryxpace@proton.me>
2026-04-15 12:54:59 +0900

    vendorsetup: remove AAPT
    - APP_MANAGER disabled by default, no need to defined it

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -47,7 +47,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
   export FOX_USE_TAR_BINARY=1
   export FOX_USE_XZ_UTILS=1
   export FOX_DELETE_AROMAFM=1
-  export FOX_ENABLE_APP_MANAGER=0
+  export FOX_REMOVE_AAPT=1
   export FOX_USE_DATE_BINARY=1
   export FOX_VANILLA_BUILD=1
 
