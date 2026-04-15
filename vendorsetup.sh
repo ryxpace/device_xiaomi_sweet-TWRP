5a31e5031c2de29820de48136d31b8631aa18676
Raynando Andre <ryxpace@proton.me>
2026-04-15 19:01:08 +0900

    vendrorsetup: Bump FOX_MAINTAINER_PATCH_VERSION to 2

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -57,7 +57,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
   # FOX Version & Variant Settings
   export FOX_VARIANT=crDroid
-  export FOX_MAINTAINER_PATCH_VERSION=1
+  export FOX_MAINTAINER_PATCH_VERSION=2
 
   # let's see what are our build VARs
   if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
