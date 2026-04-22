cc18e701aaa3b6e59a667b7a9b5ac1fb055f1782
Raynando Andre <ryxpace@proton.me>
2026-04-22 12:48:34 +0900

    vendorsetup: reformat indentation to 4 spaces

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -32,45 +32,45 @@ fox_get_target_device() {
 }
 
 if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
-  echo "** WARNING **: Always set FOX_BUILD_DEVICE to the device codename before starting to build for any device!"
-  fox_get_target_device
+    echo "** WARNING **: Always set FOX_BUILD_DEVICE to the device codename before starting to build for any device!"
+    fox_get_target_device
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-  export LC_ALL="C"
-  export ALLOW_MISSING_DEPENDENCIES=true
-  export TARGET_DEVICE_ALT="sweetin"
-  export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
-  export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-  export FOX_USE_NANO_EDITOR=1
-  export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-  export FOX_USE_BASH_SHELL=1
-  export FOX_ASH_IS_BASH=1
-  export FOX_USE_TAR_BINARY=1
-  export FOX_USE_XZ_UTILS=1
-  export FOX_DELETE_AROMAFM=1
-  export FOX_REMOVE_AAPT=1
-  export FOX_USE_DATE_BINARY=1
-  export FOX_VANILLA_BUILD=1
+    export LC_ALL="C"
+    export ALLOW_MISSING_DEPENDENCIES=true
+    export TARGET_DEVICE_ALT="sweetin"
+    export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+    export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+    export FOX_USE_NANO_EDITOR=1
+    export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+    export FOX_USE_BASH_SHELL=1
+    export FOX_ASH_IS_BASH=1
+    export FOX_USE_TAR_BINARY=1
+    export FOX_USE_XZ_UTILS=1
+    export FOX_DELETE_AROMAFM=1
+    export FOX_REMOVE_AAPT=1
+    export FOX_USE_DATE_BINARY=1
+    export FOX_VANILLA_BUILD=1
 
-  export FOX_SETTINGS_ROOT_DIRECTORY=/data/recovery
-  export FOX_MISCELLANEOUS_ROOT_DIRECTORY=/sdcard
-  export FOX_ALLOW_EARLY_SETTINGS_LOAD=1
+    export FOX_SETTINGS_ROOT_DIRECTORY=/data/recovery
+    export FOX_MISCELLANEOUS_ROOT_DIRECTORY=/sdcard
+    export FOX_ALLOW_EARLY_SETTINGS_LOAD=1
 
-  # FOX Version & Variant Settings
-  export FOX_VARIANT=crDroid
-  export FOX_MAINTAINER_PATCH_VERSION=2
+    # FOX Version & Variant Settings
+    export FOX_VARIANT=crDroid
+    export FOX_MAINTAINER_PATCH_VERSION=2
 
-  # let's see what are our build VARs
-  if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-    export | grep "FOX" >>$FOX_BUILD_LOG_FILE
-    export | grep "OF_" >>$FOX_BUILD_LOG_FILE
-    export | grep "TARGET_" >>$FOX_BUILD_LOG_FILE
-    export | grep "TW_" >>$FOX_BUILD_LOG_FILE
-  fi
+    # let's see what are our build VARs
+    if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+        export | grep "FOX" >>$FOX_BUILD_LOG_FILE
+        export | grep "OF_" >>$FOX_BUILD_LOG_FILE
+        export | grep "TARGET_" >>$FOX_BUILD_LOG_FILE
+        export | grep "TW_" >>$FOX_BUILD_LOG_FILE
+    fi
 else
-  if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
-    echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
-  fi
+    if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
+        echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
+    fi
 fi
 #
